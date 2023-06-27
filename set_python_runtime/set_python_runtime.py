f074547fc9b9a353b9028a4af484a8d7b1cc6634
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2023-06-27 19:19:38 +0200

    Fix files without .py extension
    Some python script files don't have .py extension, so it is a
    must to check all the files.

--- set_python_runtime/set_python_runtime.py
+++ set_python_runtime/set_python_runtime.py
@@ -7,20 +7,27 @@ import os
 import glob
 
 BASE_PATH = sys.argv[1]
-for full_file_path in glob.glob(os.path.join(BASE_PATH, "**/*.py"), recursive=True):
+for full_file_path in glob.glob(os.path.join(BASE_PATH, "**/*"), recursive=True):
     if not os.path.isfile(full_file_path):
         continue
-    with open(full_file_path, "r", encoding='utf-8') as file_data:
-        content = file_data.readlines()
+    with open(full_file_path, "rb") as file_data:
+        shebang = file_data.read(2)
+    if shebang != b"#!":
+        continue
+    try:
+        with open(full_file_path, "r", encoding='utf-8') as file_data:
+            content = file_data.readlines()
+    except:
+        content = []
     if len(content) == 0:
         continue
-    if content[0][:2] != '#!':
-        continue
-    if (not content[0].endswith("python") and
-            not content[0].endswith("python2") and
-            not content[0].endswith("python3")):
+    first_line = content[0].strip()
+    if (not first_line.endswith("python") and
+            not first_line.endswith("python2") and
+            not first_line.endswith("python3")):
         continue
     with open(full_file_path, "w", encoding='utf-8') as file_data:
         file_data.write("#!/usr/bin/env python3\n")
         for line in content[1:]:
             file_data.write(line)
+        print(f"Fixing file {full_file_path}")
