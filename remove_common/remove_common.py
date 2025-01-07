528db8a88f14a85de632e2bb52a1e1e1aab3d5b7
Sergio Costas <sergio.costas@canonical.com>
2025-01-07 14:51:44 +0100

    Refactored code and added extra tests (#8)
    * Refactored code and added extra tests
    
    * Fix unitary tests
    
    * Some style changes
    
    * Group tests and add mapping test
    
    * Add unitary test CI

--- remove_common/remove_common.py
+++ remove_common/remove_common.py
@@ -18,29 +18,190 @@ except:
     pass
 
 parser = argparse.ArgumentParser(prog="remove_common", description="An utility to remove from snaps files that are already available in extensions")
-parser.add_argument('extension', nargs='*', default=None)
-parser.add_argument('-e', '--exclude', nargs='+', help="A list of file and folders to exclude from checking")
-parser.add_argument('-m', '--map', nargs='+', help="A list of snap_name:path pairs")
-parser.add_argument('-v', '--verbose', action='store_true', help="Show extra info")
-parser.add_argument('-q', '--quiet', action='store_true', help="Don't show any message")
+parser.add_argument('extension', nargs='*', default=[])
+parser.add_argument('-e', '--exclude', nargs='+', help="A list of files and directories to exclude from checking")
+parser.add_argument('-m', '--map', nargs='+', default={}, help="A list of snap_name:path pairs")
+parser.add_argument('-v', '--verbose', action='store_true', default=False, help="Show extra info")
+parser.add_argument('-q', '--quiet', action='store_true', default=False, help="Don't show any message")
 args = parser.parse_args()
 
 # specific case for themed icons
 global_excludes = ['usr/share/icons/*/index.theme']
-global_maps = {"gtk-common-themes": "usr"}
+global_maps = ['gtk-common-themes:usr']
 
 def get_snapcraft_yaml():
-    base_folder = os.environ['CRAFT_PROJECT_DIR']
-    snapcraft_file_path = os.path.join(base_folder, "snapcraft.yaml")
+    """Returns a string with the full path of the snapcraft file.
+
+    Returns
+    -------
+    string
+        The full path of the snapcraft.yaml file, or None if no file is found.
+    """
+
+    if 'CRAFT_PROJECT_DIR' not in os.environ:
+        return None
+
+    project_folder = os.environ['CRAFT_PROJECT_DIR']
+    snapcraft_file_path = os.path.join(project_folder, "snapcraft.yaml")
     if not os.path.exists(snapcraft_file_path):
-        snapcraft_file_path = os.path.join(base_folder, "snap", "snapcraft.yaml")
+        snapcraft_file_path = os.path.join(project_folder, "snap", "snapcraft.yaml")
         if not os.path.exists(snapcraft_file_path):
             return None
     return snapcraft_file_path
 
-def check_if_exists(folder_list, relative_file_path, verbose):
-    """ Checks if an specific file does exist in any of the base paths"""
-    for folder, map_path in folder_list:
+
+def get_extension_list(cmdline_extensions):
+    """Returns an array with the extensions for this project.
+
+    Parameters
+    ----------
+    cmdline_extensions : array of strings
+        an array with the list of extensions passed by the command line.
+        If no extensions were passed, it must be a zero-length array.
+
+    Returns
+    -------
+    array of strings
+        an array with the list of extensions desired. If no extensions are
+        defined, it will return a zero-length array.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the snapcraft.yaml file can't be found.
+    """
+
+    if len(cmdline_extensions) != 0:
+        return cmdline_extensions
+
+    snapcraft_file = get_snapcraft_yaml()
+    if snapcraft_file is None:
+        raise FileNotFoundError("There is no snapcraft.yaml file in the project folder")
+
+    with open(snapcraft_file, "r") as snapcraft_stream:
+        snapcraft_data = yaml.load(snapcraft_stream, Loader=yaml.Loader)
+    parts_data = snapcraft_data["parts"]
+    extensions = []
+    for part_name in parts_data:
+        part_data = parts_data[part_name]
+        if "build-snaps" not in part_data:
+            continue
+        for extension in part_data["build-snaps"]:
+            if extension not in extensions:
+                extensions.append(extension)
+    return extensions
+
+
+def generate_mappings(predefined_mappings, cmdline_mappings):
+    """Parses the specific mappings for each snap
+
+    It receives both the predefined mappings and the mappings passed by
+    command line, and returns a dictionary with each snap and its corresponding
+    mapping path.
+
+    Parameters
+    ----------
+    predefined_mappings : array of strings
+        An array of strings, each one in the format snap_name:mapping. Used
+        for the "known mappings", like the one for gtk-common-themes.
+    cmdline_mappings : array of strings
+        An array of strings as received from argparse with the snap_name:mapping
+        pairs.
+
+    Returns
+    -------
+    dictionary
+        A dictionary where each key is a string with a snap name and the
+        value is another string with the mapping path for that snap, ended
+        in '/'.
+
+    Raises
+    ------
+    SyntaxError
+        If the format of any of the mapping strings is not valid
+    """
+
+    mappings = {}
+
+    for map in predefined_mappings + cmdline_mappings:
+        elements = map.split(":")
+        if len(elements) != 2:
+            raise SyntaxError("Error in mapping. It must be in the format snap_name:path", {'filename': 'remove_common.py', 'text': map, 'lineno': 0, 'offset': 0})
+        if elements[1] == '/':
+            raise SyntaxError("The mapping can't be '/'", {'filename': 'remove_common.py', 'text': map, 'lineno': 0, 'offset': 0})
+        while elements[1][0] == '/':
+            elements[1] = elements[1][1:]
+        if elements[1][-1] != '/':
+            elements[1] += '/'
+        mappings[elements[0]] = elements[1]
+
+    return mappings
+
+
+def generate_extensions_paths(extensions, mappings):
+    """Generates the list of extensions paths from the list of extensions
+
+    This list has the paths of each extension used, to know where to search
+    for duplicates. Also, it contains the corresponding map for each path.
+
+    Parameters
+    ----------
+    extensions : array of strings
+        An array of strings with the names of the extensions used in this snap.
+    mappings : dictionary
+        A dictionary where each key is a string with a snap name and the
+        value is another string with the mapping path for that snap, ended
+        in '/'.
+
+    Returns
+    -------
+    array of tuples
+        An array of tuples, where the first element of each tuple is a string
+        to the contents of each extension, and the second element is the mapping
+        for that path, or None if no mapping is needed.
+    """
+
+    folders = []
+    for snap in extensions:
+        path = f"/snap/{snap}/current"
+        map_path = mappings[snap] if snap in mappings else None
+        folders.append((path, map_path))
+    return folders
+
+
+def check_if_exists(extensions_paths, relative_file_path, verbose):
+    """Checks if an specific file does exist in any of the base paths.
+
+    Checks if the specified file at `relative_file_path` does exist in
+    any of the paths included in the `extensions_paths` array, taking into
+    account the mapping specified.
+
+    Mapping is paramount for some extension snaps like gtk-common-themes.
+    In this specific case, the snap contains the `share` folder directly
+    at its root folder, while any other snap would have it inside the
+    `usr` folder. Thus, for that extension snap, the map must be `usr`
+    to instruct this function to remove `usr` from the beginning of the
+    relative path when checking for a file inside gtk-common-themes.
+
+    Parameters
+    ----------
+    extensions_paths : array of tuples with two elements
+        An array with tuples containing each one a string with the root
+        path for one of the extensions snap, and, if required, another
+        string with the mapping for that snap (or None if no mapping is
+        required for that snap).
+    relative_file_path : string
+        The file path to search in the folder list, relative to the
+        snap root.
+
+    Returns
+    -------
+    boolean
+        It returns True if the file does exist in, at least, one of the
+        specified snaps, and false if it doesn't exist in any of them.
+    """
+    # Checks if an specific file does exist in any of the base paths
+    for folder, map_path in extensions_paths:
         if (map_path is not None) and relative_file_path.startswith(map_path):
             relative_file_path2 = relative_file_path[len(map_path):]
             if relative_file_path2[0] == '/':
@@ -55,14 +216,40 @@ def check_if_exists(folder_list, relative_file_path, verbose):
     return False
 
 
-def main(base_folder, folder_list, exclude_list, verbose=False, quiet=True):
-    """ Main function """
+def main(snap_folder, extensions_paths, exclude_list=[], verbose=False, quiet=True):
+    """Main function
+
+    Searches each file in 'snap_folder' inside each path in 'extensions_paths'
+    to check if it is already available there, deleting it in that case, unless
+    it matches any of the rules in 'exclude_list'.
+
+    The check is done based only on the relative path and file name relative to
+    'snap_folder', searching that inside each path in 'extensions_paths', although
+    taking into account the mapping.
+
+    Parameters
+    ----------
+    snap_folder : string
+        The path of the folder where the staged .deb have been uncompressed (usually
+        CRAFT_PART_INSTALL)
+    extensions_paths : array of tuples with two elements
+        An array with tuples containing each one a string with the root
+        path for one of the extensions snap, and, if required, another
+        string with the mapping for that snap (or None if no mapping is
+        required for that snap).
+    exclude_list : array of strings
+        A list of fnmatch rules for excluding files and/or paths
+    verbose : bool, optional
+        Show extra verbose information, by default False
+    quiet : bool, optional
+        Don't show messages, by default False
+    """
 
     duplicated_bytes = 0
-    for full_file_path in glob.glob(os.path.join(base_folder, "**/*"), recursive=True):
+    for full_file_path in glob.glob(os.path.join(snap_folder, "**/*"), recursive=True):
         if not os.path.isfile(full_file_path) and not os.path.islink(full_file_path):
             continue
-        relative_file_path = full_file_path[len(base_folder):]
+        relative_file_path = full_file_path[len(snap_folder):]
         if relative_file_path[0] == '/':
             relative_file_path = relative_file_path[1:]
         do_exclude = False
@@ -74,7 +261,7 @@ def main(base_folder, folder_list, exclude_list, verbose=False, quiet=True):
                 break
         if do_exclude:
             continue
-        if check_if_exists(folder_list, relative_file_path, verbose):
+        if check_if_exists(extensions_paths, relative_file_path, verbose):
             if os.path.isfile(full_file_path):
                 duplicated_bytes += os.stat(full_file_path).st_size
             os.remove(full_file_path)
@@ -85,8 +272,6 @@ def main(base_folder, folder_list, exclude_list, verbose=False, quiet=True):
 
 
 if __name__ == "__main__":
-    folders = []
-
     verbose = args.verbose
     exclude = args.exclude
     extensions = args.extension
@@ -96,58 +281,22 @@ if __name__ == "__main__":
     if exclude is not None:
         global_excludes += exclude
 
+    extensions = get_extension_list(args.extension)
     if len(extensions) == 0:
-        # get the extensions from the snapcraft file
-        snapcraft_file = get_snapcraft_yaml()
-        if snapcraft_file is None:
-            print("Failed to get the snapcraft.yaml file. Aborting")
-            sys.exit(1)
-        with open(snapcraft_file, "r") as snapcraft_stream:
-            snapcraft_data = yaml.load(snapcraft_stream, Loader=yaml.Loader)
-        parts_data = snapcraft_data["parts"]
-        extensions = []
-        for part_name in parts_data:
-            part_data = parts_data[part_name]
-            if "build-snaps" not in part_data:
-                continue
-            for extension in part_data["build-snaps"]:
-                if extension not in extensions:
-                    extensions.append(extension)
+        print("Called remove_common.py without a list of snaps, and no 'build-snaps' entry in the snapcraft.yaml file. Aborting.")
+        sys.exit(1)
 
-        if len(extensions) == 0:
-            print("Called remove_common.py without a list of snaps, and no 'build-snaps' entry in the snapcraft.yaml file. Aborting.")
-            sys.exit(1)
-
-    # specific case for gtk-common-themes
-    for snap in global_maps:
-        if snap in extensions:
-            mapping[snap] = global_maps[snap]
-
-    if args.map is not None:
-        for map in args.map:
-            elements = map.split(":")
-            if len(elements) != 2:
-                print(f"Error in mapping ${map}. It must be in the format snap:path")
-                sys.exit(1)
-            if elements[0] not in extensions:
-                print(f"Warning: The mapping '{map}' points to an undefined extension")
-            if elements[1] == '/':
-                print(f"Invalid mapping for {elements[0]} (can't begin with '/')")
-                sys.exit(2)
-            while elements[1][0] == '/':
-                elements[1] = elements[1][1:]
-            if elements[1][-1] != '/':
-                elements[1] += '/'
-            mapping[elements[0]] = elements[1]
+    mappings = generate_mappings(global_maps, args.map)
 
     if verbose:
         print(f"Removing duplicates already in {extensions}")
-    folders.append((os.environ["CRAFT_STAGE"], None))
-    for snap in extensions:
-        path = f"/snap/{snap}/current"
-        map_path = mapping[snap] if snap in mapping else None
-        folders.append((path, map_path))
 
-    install_folder = os.environ["CRAFT_PART_INSTALL"]
+    extensions_paths = generate_extensions_paths(extensions, mappings)
+    extensions_paths.append((os.environ["CRAFT_STAGE"], None))
 
-    main(install_folder, folders, global_excludes, verbose, quiet)
+    # This is the folder where to check for duplicates that are already
+    # in other snaps, or in the stage because they were built in other
+    # parts.
+    snap_folder = os.environ["CRAFT_PART_INSTALL"]
+
+    main(snap_folder, extensions_paths, global_excludes, verbose, quiet)
--- remove_common/unittests.py
+++ remove_common/unittests.py
@@ -32,9 +32,9 @@ class TestRemoveCommon(unittest.TestCase):
         self._touch_file("core22/usr/bin/file1.txt")
         self._touch_file("core22/usr/bin/file2.txt")
         self._touch_file("core22/usr/lib/file1.txt")
-        self._touch_file("gnome-42-2204/usr/bin/file1.txt")
-        self._touch_file("gnome-42-2204/usr/share/file1.txt")
-        self._touch_file("gnome-42-2204/usr/share/file2.txt")
+        self._touch_file("gnome-42-2204/bin/file1.txt")
+        self._touch_file("gnome-42-2204/share/file1.txt")
+        self._touch_file("gnome-42-2204/share/file2.txt")
 
         final_files = []
         final_files.append(self._touch_file("stage/usr/bin/file1.txt"))
@@ -51,95 +51,22 @@ class TestRemoveCommon(unittest.TestCase):
     def _delete_files(self):
         shutil.rmtree(self._base_test_folder)
 
-    def test_basic_test0(self):
+    def test_basic_test(self):
         """ This test creates several files in two emulated snaps, and
             some repeated and non-repeated files in an emulated stage,
             and checks that only the duplicated files are removed. """
 
         final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
+        remove_common.main("/tmp/tests/stage", [("/tmp/tests/core22", None), ("/tmp/tests/gnome-42-2204", "usr/")])
         assert not os.path.exists(final_files[0])
-        self._delete_files()
-
-    def test_basic_test1(self):
-        """ This test creates several files in two emulated snaps, and
-            some repeated and non-repeated files in an emulated stage,
-            and checks that only the duplicated files are removed. """
-
-        final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
         assert not os.path.exists(final_files[1])
-        self._delete_files()
-
-    def test_basic_test2(self):
-        """ This test creates several files in two emulated snaps, and
-            some repeated and non-repeated files in an emulated stage,
-            and checks that only the duplicated files are removed. """
-
-        final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
         assert os.path.exists(final_files[2])
-        self._delete_files()
-
-    def test_basic_test3(self):
-        """ This test creates several files in two emulated snaps, and
-            some repeated and non-repeated files in an emulated stage,
-            and checks that only the duplicated files are removed. """
-
-        final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
         assert not os.path.exists(final_files[3])
-        self._delete_files()
-
-    def test_basic_test4(self):
-        """ This test creates several files in two emulated snaps, and
-            some repeated and non-repeated files in an emulated stage,
-            and checks that only the duplicated files are removed. """
-
-        final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
         assert os.path.exists(final_files[4])
-        self._delete_files()
-
-    def test_basic_test5(self):
-        """ This test creates several files in two emulated snaps, and
-            some repeated and non-repeated files in an emulated stage,
-            and checks that only the duplicated files are removed. """
-
-        final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
         assert not os.path.exists(final_files[5])
-        self._delete_files()
-
-    def test_basic_test6(self):
-        """ This test creates several files in two emulated snaps, and
-            some repeated and non-repeated files in an emulated stage,
-            and checks that only the duplicated files are removed. """
-
-        final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
         assert not os.path.exists(final_files[6])
-        self._delete_files()
-
-    def test_basic_test7(self):
-        """ This test creates several files in two emulated snaps, and
-            some repeated and non-repeated files in an emulated stage,
-            and checks that only the duplicated files are removed. """
-
-        final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
         assert os.path.exists(final_files[7])
-        self._delete_files()
-
-    def test_basic_test8(self):
-        """ This test creates several files in two emulated snaps, and
-            some repeated and non-repeated files in an emulated stage,
-            and checks that only the duplicated files are removed. """
-
-        final_files = self._create_files()
-        remove_common.main("/tmp/tests/stage", ["/tmp/tests/core22", "/tmp/tests/gnome-42-2204"])
         assert os.path.exists(final_files[8])
         self._delete_files()
-
 
 unittest.main()
